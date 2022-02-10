ca12b3d0b6d9f2769b70fd63851378905507612a
Mendoza Navarro Brandon Abraham <99433383+Brandon-Abraham-Mendoza-Navarro-UABC@users.noreply.github.com>
2022-02-10 11:18:55 -0800

    Add files via upload

---
+++ Hola Mundo Python.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("\nHola mundoooo");
+nombre = input("Escribe tu nombre: ");
+print("\nHola mundo, me llamo", nombre);
